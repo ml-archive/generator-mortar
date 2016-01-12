e78b096f8aa3fb105d7ab052d09db4f13239c330
Kyle Mendes <kmendes@fuzzproductions.com>
2016-01-12 11:54:17 -0500

    Cleaning up templates

--- app/templates/src/menu.js
+++ app/templates/src/menu.js
@@ -1,7 +1,7 @@
 'use strict';
 
 $(function() {
-    $('#side-menu').metisMenu();
+	$('#side-menu').metisMenu();
 
 });
 
@@ -9,31 +9,31 @@ $(function() {
 //collapses the sidebar on window resize.
 // Sets the min-height of #page-wrapper to window size
 $(function() {
-    $(window).bind("load resize", function() {
-        var topOffset = 50;
-        var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
-        if (width < 768) {
-            $('div.navbar-collapse').addClass('collapse');
-            topOffset = 100; // 2-row-menu
-        } else {
-            $('div.navbar-collapse').removeClass('collapse');
-        }
+	$(window).bind("load resize", function() {
+		var topOffset = 50;
+		var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+		if (width < 768) {
+			$('div.navbar-collapse').addClass('collapse');
+			topOffset = 100; // 2-row-menu
+		} else {
+			$('div.navbar-collapse').removeClass('collapse');
+		}
 
-        var height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
-        height = height - topOffset;
-        if (height < 1) {
+		var height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
+		height = height - topOffset;
+		if (height < 1) {
 			height = 1;
 		}
-        if (height > topOffset) {
-            $("#page-wrapper").css("min-height", (height) + "px");
-        }
-    });
+		if (height > topOffset) {
+			$("#page-wrapper").css("min-height", (height) + "px");
+		}
+	});
 
-    var url = window.location;
-    var element = $('ul.nav a').filter(function() {
-        return this.href === url || url.href.indexOf(this.href) === 0;
-    }).addClass('active').parent().parent().addClass('in').parent();
-    if (element.is('li')) {
-        element.addClass('active');
-    }
+	var url = window.location;
+	var element = $('ul.nav a').filter(function() {
+		return this.href === url || url.href.indexOf(this.href) === 0;
+	}).addClass('active').parent().parent().addClass('in').parent();
+	if (element.is('li')) {
+		element.addClass('active');
+	}
 });
